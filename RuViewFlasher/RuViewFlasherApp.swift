14f464ee4a14c205ffd30e967520df9e9c4e74e6
steven <smearles@gmail.com>
2026-06-28 15:31:41 -0500

    Initial Commit

---
+++ RuViewFlasher/RuViewFlasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuViewFlasherApp.swift
+//  RuViewFlasher
+//
+//  Created by user on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RuViewFlasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
